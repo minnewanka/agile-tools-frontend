acfdec9f1a31bef59bed7f5e05bdacee6a0a9947
Philippe INTHAVONG <philippe.inthavong@SIICANADA.COM>
2019-03-21 14:36:38 -0400

    sending mail fonction

--- src/common/components/contactUs/contactUs.jsx
+++ src/common/components/contactUs/contactUs.jsx
@@ -1,61 +1,122 @@
-import React from "react"
+import React, { Component } from "react"
 import { Modal, Row, Input, Button } from "react-materialize"
 import "./contactUs.scss"
+import Parse from "parse"
 
-const ContactUs = ({ translate }) => {
-  return (
-    <Modal
-      header={translate("contact")}
-      className="contact-us-container"
-      trigger={(
-<button type="button" className="contact-us-btn button-noStyle">
-          {translate("contact")}
-        </button>
-)}
-      actions={(
-<div className="actions-buttons-container">
-          <Button
-            flat
-            modal="close"
-            waves="light"
-            className="actions-button-cancel"
-          >
-            {translate("btn.cancel")}
-          </Button>
-          <Button waves="light" className="btn-sii">
-            {translate("btn.send")}
-          </Button>
-        </div>
-)}
-    >
-      <Row>
-        <Input s={6} label={translate("firstname")} />
-        <Input s={6} label={translate("lastname")} />
-        <Input type="email" label={translate("email")} s={6} />
-        <Input s={6} label={translate("company")} />
-      </Row>
-      <Row>
-        <div className="comment-section">
+class ContactUs extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstname: "",
+      lastname: "",
+      email: "",
+      company: "",
+      subject: "",
+      message: ""
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.sendFeedback = this.sendFeedback.bind(this)
+  }
+
+  sendFeedback = () => {
+    Parse.Cloud.run("sendFeedback", this.state)
+  }
+
+  handleInputChange(event) {
+    const { target } = event
+    const { name } = target
+    const value = target.type === "checkbox" ? target.checked : target.value
+    this.setState({
+      [name]: value
+    })
+    console.log(this.state)
+  }
+
+  render() {
+    const { translate } = this.props
+    return (
+      <Modal
+        header={translate("contact")}
+        className="contact-us-container"
+        trigger={
+          <button type="button" className="contact-us-btn button-noStyle">
+            {translate("contact")}
+          </button>
+        }
+        actions={
+          <div className="actions-buttons-container">
+            <Button
+              flat
+              modal="close"
+              waves="light"
+              className="actions-button-cancel"
+            >
+              {translate("btn.cancel")}
+            </Button>
+            <Button
+              modal="close"
+              waves="light"
+              className="btn-sii"
+              onClick={this.sendFeedback}
+            >
+              {translate("btn.send")}
+            </Button>
+          </div>
+        }
+      >
+        <Row>
           <Input
-            className="subject"
-            type="select"
-            label={translate("subject")}
-            defaultValue={translate("comment")}
-            s={3}
-          >
-            <option value="comment">{translate("comment")}</option>
-            <option value="bug">{translate("bug")}</option>
-          </Input>
-          <Input
-            label={translate("message")}
-            type="textarea"
-            // placeholder="Décrivez votre problème ou partagez vos idées"
-            s={12}
+            name="firstname"
+            s={6}
+            label={translate("firstname")}
+            onChange={this.handleInputChange}
           />
-        </div>
-      </Row>
-    </Modal>
-  )
+          <Input
+            name="lastname"
+            s={6}
+            label={translate("lastname")}
+            onChange={this.handleInputChange}
+          />
+          <Input
+            name="email"
+            type="email"
+            label={translate("email")}
+            s={6}
+            onChange={this.handleInputChange}
+          />
+          <Input
+            name="company"
+            s={6}
+            label={translate("company")}
+            onChange={this.handleInputChange}
+          />
+        </Row>
+        <Row>
+          <div className="comment-section">
+            <Input
+              name="subject"
+              className="subject"
+              type="select"
+              label={translate("subject")}
+              defaultValue={translate("comment")}
+              s={3}
+              onChange={this.handleInputChange}
+            >
+              <option value="comment">{translate("comment")}</option>
+              <option value="bug">{translate("bug")}</option>
+            </Input>
+            <Input
+              name="message"
+              label={translate("message")}
+              type="textarea"
+              s={12}
+              onChange={this.handleInputChange}
+            />
+          </div>
+        </Row>
+      </Modal>
+    )
+  }
 }
 
 export default ContactUs
