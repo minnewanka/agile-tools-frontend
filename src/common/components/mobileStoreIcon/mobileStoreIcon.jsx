64eece91ce7fb850c08700ed4e9f60571241ef75
Philippe INTHAVONG <philippe.inthavong@SIICANADA.COM>
2019-04-10 13:03:06 -0400

    change appstore url

--- src/common/components/mobileStoreIcon/mobileStoreIcon.jsx
+++ src/common/components/mobileStoreIcon/mobileStoreIcon.jsx
@@ -20,7 +20,7 @@ const MobileStoreIcon = ({ locale }) => (
     </div>
     <div className="apple-badge-container">
       <a
-        href="https://www.siicanada.com/fr/"
+        href="https://itunes.apple.com/us/app/urbanspoon/id1454388380"
         target="_blank"
         rel="noopener noreferrer"
       >
