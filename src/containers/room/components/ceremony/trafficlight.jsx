b2a74c7a68f9decdf96f6dffaed30d8b463aff92
Philippe INTHAVONG <philippe.inthavong@SIICANADA.COM>
2019-04-11 14:19:47 -0400

    add shake when flipped and value change

--- src/containers/room/components/ceremony/trafficlight.jsx
+++ src/containers/room/components/ceremony/trafficlight.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './trafficlight.scss'
 import { ReactComponent as TrafficLightSVG } from '../../../../img/traffic_light.svg'
 
-const TrafficLight = props => {
-  const { isFlipped, vote, username } = props
-  return (
-    <div className="trafficlight-container">
-      <TrafficLightSVG
-        className={`trafficlight ${!isFlipped ? `trafficlight-${vote}` : ''}`}
-      />
-      <span className="username">{username}</span>
-    </div>
-  )
+class TrafficLight extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      shake: false
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { vote } = this.props
+    if (vote !== prevProps.vote) {
+      // setState in an if claue is OK
+      /* eslint-disable react/no-did-update-set-state */
+      this.setState({ shake: true })
+    }
+  }
+
+  render() {
+    const { isFlipped, vote, username } = this.props
+    const { shake } = this.state
+    return (
+      <div
+        className={`trafficlight-container ${
+          shake && isFlipped ? 'wobble-hor-bottom' : ''
+        }`}
+        onAnimationEnd={() => this.setState({ shake: false })}
+      >
+        <TrafficLightSVG
+          className={`trafficlight ${!isFlipped ? `trafficlight-${vote}` : ''}`}
+        />
+        <span className="username">{username}</span>
+      </div>
+    )
+  }
 }
 
 export default TrafficLight
