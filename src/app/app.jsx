becef549297a620187027c9acb9e6e6f7e73aa6f
Philippe INTHAVONG <philippe.inthavong@SIICANADA.COM>
2019-04-18 10:56:04 -0400

    add config

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -4,7 +4,7 @@ import Routes from '../common/components/routing/routing'
 import './app.scss'
 import '../common/sass/style.scss'
 
-Parse.initialize('SIIAG')
+Parse.initialize(process.env.REACT_APP_APP_ID)
 Parse.serverURL = process.env.REACT_APP_PARSE_SERVER
 
 const App = () => (
